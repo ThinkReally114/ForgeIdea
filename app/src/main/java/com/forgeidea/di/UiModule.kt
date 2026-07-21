e6d0c6199864b06a51030d8d3ceb54ca8a1dffb6
ThinkReally114 <thinkreally114@users.noreply.github.com>
2026-07-21 11:58:51 +0000

    fix: 在 UiModule 注册 TerminalViewModel 和 GitViewModel

--- app/src/main/java/com/forgeidea/di/UiModule.kt
+++ app/src/main/java/com/forgeidea/di/UiModule.kt
@@ -3,7 +3,9 @@ package com.forgeidea.di
 import com.forgeidea.domain.usecase.SendMessageUseCase
 import com.forgeidea.tools.ExecuteCommandTool
 import com.forgeidea.ui.chat.ChatViewModel
+import com.forgeidea.ui.git.GitViewModel
 import com.forgeidea.ui.settings.SettingsViewModel
+import com.forgeidea.ui.terminal.TerminalViewModel
 import com.forgeidea.ui.theme.ThemeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -13,5 +15,7 @@ val uiModule = module {
     single { ExecuteCommandTool(get()) }
     single { ThemeViewModel(get()) }
     viewModel { ChatViewModel(get(), get(), get(), get()) }
+    viewModel { TerminalViewModel(get()) }
+    viewModel { GitViewModel(get()) }
     viewModel { SettingsViewModel(get(), get()) }
 }
